7a83494a2e83689500861c54b78e122e074fd1b0
Matty Wang <mattywang7@gmail.com>
2021-03-29 18:12:21 +0100

    worker finished

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,9 +1,11 @@
 package mr
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 import "log"
 import "net/rpc"
@@ -29,6 +31,7 @@ func ihash(key string) int {
 
 //
 // main/mrworker.go calls this function.
+// set the mapf and reducef function
 //
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
@@ -94,15 +97,86 @@ func (w *worker) doMapTask(t *Task) {
 	if err != nil {
 		w.reportTask(t, false, err)
 	}
+
+	// store the intermediate files in reduces[][]
+	kvs := w.mapf(t.FileName, string(contents))
+	reduces := make([][]KeyValue, t.NReduce)
+	for _, kv := range kvs {
+		index := ihash(kv.Key) % t.NReduce
+		reduces[index] = append(reduces[index], kv)
+	}
+
+	// create an intermediate file for each index in reduces[][]
+	for index, line := range reduces {
+		fileName := reduceName(t.Seq, index) // t.Seq = mapId, index = reduceId
+		f, err := os.Create(fileName)
+		if err != nil {
+			w.reportTask(t, false, err)
+			return
+		}
+		encoder := json.NewEncoder(f)
+		for _, kv := range line {
+			if err := encoder.Encode(&kv); err != nil {
+				w.reportTask(t, false, err)
+				// not return, get an uncompleted intermediate file
+			}
+		}
+		if err := f.Close(); err != nil {
+			w.reportTask(t, false, err)
+		}
+	}
+	w.reportTask(t, true, nil)
 }
 
 func (w *worker) doReduceTask(t *Task) {
-	// TODO
+	// maps store key string, values []string
+	maps := make(map[string][]string)
+	// read NMap intermediate files from map workers
+	for index := 0; index < t.NMap; index++ {
+		// each file contain []KeyValue with kvs having the same Key
+		fileName := reduceName(index, t.Seq)
+		file, err := os.Open(fileName)
+		if err != nil {
+			w.reportTask(t, false, err)
+			return
+		}
+		decoder := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := decoder.Decode(&kv); err != nil {
+				break
+			}
+			if _, ok := maps[kv.Key]; !ok {
+				maps[kv.Key] = make([]string, 0, 100)
+			}
+			maps[kv.Key] = append(maps[kv.Key], kv.Value)
+		}
+	}
+	res := make([]string, 0, 100)
+	for k, v := range maps {
+		res = append(res, fmt.Sprintf("%v: %v\n", k, w.reducef(k, v)))
+	}
+	if err := ioutil.WriteFile(mergeName(t.Seq), []byte(strings.Join(res, "")), 0600); err != nil {
+		w.reportTask(t, false, err)
+	}
+	w.reportTask(t, true, nil)
 }
 
 // helper function reportTask
 func (w *worker) reportTask(t *Task, done bool, err error) {
-	// TODO
+	if err != nil {
+		log.Printf("%v", err)
+	}
+
+	args := ReportTaskArgs{}
+	args.Done = done
+	args.Phase = t.Phase
+	args.WorkerId = w.id
+	args.Seq = t.Seq
+	reply := ReportTaskReply{}
+	if ok := call("Coordinator.ReportTask", &args, &reply); !ok {
+		DPrintf("report task fail: %+v", args)
+	}
 }
 
 // register ask the Coordinator to get a workerId
